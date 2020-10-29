4b90419bbf43e65636dee70805c8c698e8a101b7
skitol <kkalwarski@gmail.com>
2020-10-29 17:45:14 -0400

    better register logging

--- scripts/register.sh
+++ scripts/register.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 # shellcheck disable=SC2207
-PROJECTS=( $(jq -r '.projects[] | select(.type == "application") | .root' ./nest-cli.json) )
+PROJECTS=($(jq -r '.projects[] | select(.type == "application") | .root' ./nest-cli.json))
 BOOTSTRAP_PATH=src/bootstrap-development.yaml
-CONFIG_PATH=config.example
-
+CONFIG_PATH_PRIVATE=config.yaml
+CONFIG_PATH_DEFAULT=config.example
 echo "Service Registration system started"
 
-for PROJECT_DIR in "${PROJECTS[@]}" ; do
+for PROJECT_DIR in "${PROJECTS[@]}"; do
+  echo "\n-----"
+  echo "Checking: ${PROJECT_DIR}"
   if [ ! -f "./${PROJECT_DIR}/${BOOTSTRAP_PATH}" ]; then
     echo "./${PROJECT_DIR}/${BOOTSTRAP_PATH} not found, skipping service"
     continue
   fi
 
-  SVC_NAME=$(yq r ./${PROJECT_DIR}/${BOOTSTRAP_PATH} 'service.name' )
-  echo "Registering ${SVC_NAME}"
+  SVC_NAME=$(yq r ./${PROJECT_DIR}/${BOOTSTRAP_PATH} 'service.name')
 
-  if [ ! -f "./${PROJECT_DIR}/${CONFIG_PATH}" ]; then
-    echo "./${PROJECT_DIR}/${CONFIG_PATH} not found, skipping service"
-    continue
+  if [ ! -f "./${PROJECT_DIR}/${CONFIG_PATH_PRIVATE}" ]; then
+    CONFIG_PATH=${CONFIG_PATH_DEFAULT}
+  else
+    CONFIG_PATH=${CONFIG_PATH_PRIVATE}
   fi
 
-    echo "**** ${PROJECT_DIR}"
+  echo "Service: ${SVC_NAME}"
+  echo "Config: ${CONFIG_PATH}"
+
+  if [ ! -f "./${PROJECT_DIR}/${CONFIG_PATH}" ]; then
+    echo "ERROR: ./${PROJECT_DIR}/${CONFIG_PATH} file not found."
+    continue
+  fi
 
   consul kv put ultimatebackend/config/${SVC_NAME} \@./${PROJECT_DIR}/${CONFIG_PATH}
 done
 
-echo "Service Registration system completed"
+echo "\nService Registration system completed!"
