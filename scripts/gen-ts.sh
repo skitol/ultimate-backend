cc5217939dc9213c3d88c63c99d7a538a8e83c64
skitol <kkalwarski@gmail.com>
2020-10-29 17:44:12 -0400

    move ts-gen to scripts

--- scripts/gen-ts.sh
+++ scripts/gen-ts.sh
@@ -17,6 +17,8 @@ DEST_DIR="./libs/proto-schema/src"
 
 protoc --plugin=node_modules/ts-proto/protoc-gen-ts_proto --ts_proto_opt=outputEncodeMethods=true,useEnumNames=false,asClass=false,outputJsonMethods=true,context=true,outputNestJs=true,outputClientImpl=false --ts_proto_out=${DEST_DIR} ${SRC_DIR}
 
+node_modules/.bin/prettier --write libs/proto-schema/**/*.ts
+
 # protobuf.js
 # node_modules/.bin/pbjs \
 # -t static-module \
